9195fc6b243ef37b80d11850acf8bbff1ee9aca2
Mohammed Ahmed Khan <ahmedkhanm565@gmail.com>
2026-09-24 15:58:21 +0530

    Allow Render host

--- profolio/settings.py
+++ profolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-()57*i)t0t#a4zmq2tf#qqja_4%#7i_^o0tu$7jpjt@m%xnj!!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["portfolio-spnc.onrender.com"]
 
 
 # Application definition
